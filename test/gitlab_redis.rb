e70600af010951fa3360ee190c24d4e44985e4e8
Vesa-Pekka Palmu <vpalmu@depili.fi>
2018-06-26 20:27:15 +0300

    Redis from container in gitlab CI

---
+++ test/gitlab_redis.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# We have several gems (cashier, resque) using redis
+# So set the redis for all of them here
+if Rails.env.test?
+  Rails.application.config.x.redis = { host: "redis", port: 6379, db: 2 }
+else
+  Rails.application.config.x.redis = { host: "redis", port: 6379 }
+end
+
+# Create a redis connection pool for cross-process messages
+Rails.application.config.x.redis_pool = ConnectionPool.new(size: 20, timeout: 5) { Redis.new(Rails.configuration.x.redis) }
